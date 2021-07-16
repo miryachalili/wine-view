c172e58625da7f89ae956dee40f9318e85122be4
miryachalili <87088851+miryachalili@users.noreply.github.com>
2021-07-16 10:03:41 +0300

    עדכון

--- src/App.js
+++ src/App.js
@@ -6,17 +6,28 @@ import Questionnaire from './Questionnaire';
 
 import "./App.css"
 import {Link,Route,Switch} from "react-router-dom";
-function App() {
+function App() { 
+  const style={
+       textDecoration:"none",
+       color:'black'
+    }
   return (
     <div id="app">
-       <nav id="nav">
-         <p className="nav1">  <Link to="/home">בית</Link></p>
-         <p className="nav1"><Link to="/user">הרשמה</Link> </p>
-         <p className="nav1"><Link to="/recipe">מתכונים</Link> </p>
-         <p className="nav1"><Link to="/product">מוצרים</Link> </p>
-         <p className="nav1"><Link to="/questionnaire">שאלון היין</Link> </p>
-
-       </nav>
+     <div className="headerMainContainer">
+       <div className="logo">
+       </div>
+       <div className="search">
+              <input id="searchInput" type="text" placeholder="חיפוש…" />
+              <i id="searchIcon" >icon</i>
+      </div>
+     </div>
+     <ul id="nav">
+       <li className="nav1"><Link style={style} to="/home">בית</Link></li>
+       <li className="nav1"><Link style={style} to="/user">הרשמה</Link> </li>
+       <li className="nav1"><Link style={style} to="/recipe">מתכונים</Link> </li>
+       <li className="nav1"><Link style={style} to="/product">מוצרים</Link> </li>
+       <li className="nav1"><Link style={style} to="/questionnaire">שאלון היין</Link> </li>
+     </ul>
     <Switch>
       <Route path="/user"> <User/> </Route>
       <Route path="/home"> <Home/>  </Route>
@@ -26,6 +37,10 @@ function App() {
       <Route exact path="/"> <Home/> </Route>
       <Route  path="/"> <p>not finde!!</p></Route>
    </Switch>
+     <div id="endPage">
+          <span> אזהרה :מכיל אלכוהול יש להמנע משתייה מופרזת</span>
+     </div>
+    
    </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./User.css";
-
+import image from "./images/p.jpg";
 export default class Login extends React.Component {
 
     // satate ??? האם יש צורך ב 
@@ -12,7 +12,6 @@ export default class Login extends React.Component {
     address="";
     houseNumber=0;
     //city="";--נפתח גולל של ערים
-    //bornDate="";--פתח יומן לבחירת יום חודש ותאריך
     changeFirstName = (e) => {
         this.firstName=(e.target.value);
     }
@@ -34,35 +33,39 @@ export default class Login extends React.Component {
     changeHouseNumber = (e) => {
         this.houseNumber=(e.target.value);
     }
-    checkPassword = (eemail,password) => {
-     
-
-        
+    checkPassword = (email,password) => {
+          
     }
     render() {
 
         return (
+        
+             
             <div className="g">
-                <form>
-                <h3>פרטי משתמש:</h3>
+               <div>
+                   <h1>יצירת משתמש</h1>
+                </div>
 
-                <input type="text" placeholder="שם פרטי"  onKeyUp={this.changeFirstName}/><br/>
+                <form>
+                <h3>פרטי משתמש</h3>
+            
+                <input   placeholder="שם פרטי"  onKeyUp={this.changeFirstName}/>
                 <input type="text" placeholder="שם משפחה" onKeyUp={this.changeLastName}/><br/>
-                <span>הכנס תאריך לידה:</span><br/>
-                <input type="date" /><br/>
+               
+                <input type="date" />
                  
                 <input type="tel" placeholder="טלפון"  onKeyUp={this.changePhone}/><br/>
                  {/*עיר גלילה*/}
 
-                <input type="text" placeholder="רחוב" onKeyUp={this.changeAddress}/><br/>
+                <input type="text" placeholder="רחוב" onKeyUp={this.changeAddress}/>
                 <input type="text" placeholder="מספר בית" onKeyUp={this.changeAddress}/><br/>
 
-                <h3>יצירת סיסמא:</h3>
+                <h3>יצירת סיסמא</h3>
                 <input type="email" placeholder="כתובת אימייל" onKeyUp={this.changeEmail}/><br/>
 
                {/*צורך בבדיקות תקינות לסיסמא*/}
-                <input type="text" placeholder="הכנס סיסמא"  onKeyUp={this.checkPassword}/><br/>
-                <input type="submit" value="הרשם" onClick={() => { this.checkPassword(this.email,this.password) }}/> 
+                <input type="text" placeholder="הכנס סיסמא"   onKeyUp={this.checkPassword}/><br/>
+                <button type="button"  onClick={() => { this.checkPassword(this.email,this.password) }}>הרשם</button>
 
                 </form>
             </div>
