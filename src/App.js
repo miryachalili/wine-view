1c3a2b1014bb46c9b209276d3fcb81e94d6905e4
miryachalili <87088851+miryachalili@users.noreply.github.com>
2021-08-13 10:36:51 +0300

    Revert "Merge branch 'main' of https://github.com/miryachalili/wine-view into main"
    This reverts commit 4aecbda5923e3269003e8bf4c40700219aba2e8e, reversing
    changes made to 730601f531705f5f1190e2f255c435e8f11b6a76.

--- src/App.js
+++ src/App.js
@@ -3,34 +3,25 @@ import Home from './Home';
 import Recipe from './Recipe';
 import Product from './Product';
 import Questionnaire from './Questionnaire';
-import logo from "./images/logo.jpg";
-import { GoSearch } from "react-icons/go";
 
 
 import "./App.css"
 import {Link,Route,Switch} from "react-router-dom";
-
 function App() { 
   const style={
        textDecoration:"none",
        color:'black'
     }
-
-  //  const arr=[{id:"8",name:"בני ברק"}];
   return (
     <div id="app">
-      <div id="header">
-         <img src={logo} ></img>
-          <div className="search">
+     <div className="headerMainContainer">
+       <div className="logo">
+       </div>
+       <div className="search">
               <input id="searchInput" type="text" placeholder="חיפוש…" />
-              <i onClick={()=>alert('fff')} id="searchIcon" ><GoSearch/></i>
-              
+              <i id="searchIcon" >icon</i>
       </div>
-      </div>
-     
-     {/* <select>
-     {arr.map(x=><option key={x.id} value={x.id}>{x.name}</option>)}
-     </select> */}
+     </div>
      <ul id="nav">
        <li className="nav1"><Link style={style} to="/home">בית</Link></li>
        <li className="nav1"><Link style={style} to="/user">הרשמה</Link> </li>
@@ -38,7 +29,6 @@ function App() {
        <li className="nav1"><Link style={style} to="/product">מוצרים</Link> </li>
        <li className="nav1"><Link style={style} to="/questionnaire">שאלון היין</Link> </li>
      </ul>
-
     <Switch>
       <Route path="/user"> <User/> </Route>
       <Route path="/home"> <Home/>  </Route>
