c1591aa2c7de04fddf64bec3edef643bd30a76c4
miryachalili <87088851+miryachalili@users.noreply.github.com>
2021-08-13 10:36:55 +0300

    Revert "Merge branch 'main' of https://github.com/miryachalili/wine-view into main"
    This reverts commit 730601f531705f5f1190e2f255c435e8f11b6a76, reversing
    changes made to b91b245b5e03c4a0a16d32d760ceeb31f2ccd593.

--- src/User.js
+++ src/User.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./User.css";
-import image from "./images/p.jpg";
+
 export default class Login extends React.Component {
 
     // satate ??? האם יש צורך ב 
@@ -12,6 +12,7 @@ export default class Login extends React.Component {
     address="";
     houseNumber=0;
     //city="";--נפתח גולל של ערים
+    //bornDate="";--פתח יומן לבחירת יום חודש ותאריך
     changeFirstName = (e) => {
         this.firstName=(e.target.value);
     }
@@ -33,8 +34,10 @@ export default class Login extends React.Component {
     changeHouseNumber = (e) => {
         this.houseNumber=(e.target.value);
     }
-    checkPassword = (email,password) => {
-          
+    checkPassword = (eemail,password) => {
+     
+
+        
     }
     render() {
 
@@ -48,7 +51,8 @@ export default class Login extends React.Component {
 
                 <form>
                 <h3>פרטי משתמש</h3>
-            
+               
+                     
                 <input   placeholder="שם פרטי"  onKeyUp={this.changeFirstName}/>
                 <input type="text" placeholder="שם משפחה" onKeyUp={this.changeLastName}/><br/>
                
