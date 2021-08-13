da6309a4c6119bdd16dabe42fdf21ae0f3f4a8c9
noadoan <85180712+noadoan@users.noreply.github.com>
2021-08-13 10:32:16 +0300

    Revert "Merge branch 'main' of https://github.com/miryachalili/wine-view into main"
    This reverts commit 4aecbda5923e3269003e8bf4c40700219aba2e8e, reversing
    changes made to 730601f531705f5f1190e2f255c435e8f11b6a76.

--- src/Product.js
+++ src/Product.js
@@ -1,28 +1,12 @@
 import React from "react";
-import axios  from "axios";
 
 export default class Product extends React.Component {
 
-
-    componentDidMount()
-    {
-       axios.get("http://localhost:62979/api/Users").then(x=>{
-           console.log(x.data)
-           alert(x.data.length);
-           
-        }).catch(x=>{}).finally(()=>{});
-    }
     render() {
 
-        return (<>
+        return (
             <h3>product</h3>
-           {/* <div>
-                {
-               this.state.arr.map((item, index) => {
-                         return <Square key={index} ind={index} myContent={item} toggloFunc={this.toggle} />;
-                    })}
-            </div> */}
-            </>
+        
         );
     }
 
